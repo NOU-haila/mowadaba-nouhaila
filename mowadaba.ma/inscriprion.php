f5e3520a36b0a599b3038dcac62fe1aa222dca03
Nouhaila <117626009+NOU-haila@users.noreply.github.com>
2023-10-04 20:08:51 +0100

    Add files via upload

--- mowadaba.ma/inscriprion.php
+++ mowadaba.ma/inscriprion.php
@@ -3,13 +3,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nom = $_POST["nom"];
     $prenom = $_POST["prenom"];
     $date_naissance = $_POST["date_naissance"];
+    $email=$_post["email"];
+    $con = new mysqli("localhost","root","","mowadaba.ma");
    
-    
+
+   
+    if($con->connect_error)
+die("error connetion");
   
     echo "Inscription réussie pour $prenom $nom.";
 } else {
     echo "Accès non autorisé.";
 }
+
+
+
+
+    
+
+   
 ?>
 
 
