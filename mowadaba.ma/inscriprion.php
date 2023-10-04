431fbcba076e91384d171cabfb49d159efd671f7
Nouhaila <117626009+NOU-haila@users.noreply.github.com>
2023-10-04 19:47:51 +0100

    Add files via upload

--- mowadaba.ma/inscriprion.php
+++ mowadaba.ma/inscriprion.php
@@ -3,13 +3,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nom = $_POST["nom"];
     $prenom = $_POST["prenom"];
     $date_naissance = $_POST["date_naissance"];
-    $sexe = $_POST["sexe"];
-    $classe = $_POST["classe"];
-    // Récupérer les autres données ici (e-mail, numéro de téléphone, etc.)
-
-    // Traitement des données (vous pouvez les stocker dans une base de données ou un fichier CSV)
+   
     
-    // Afficher un message de confirmation
+  
     echo "Inscription réussie pour $prenom $nom.";
 } else {
     echo "Accès non autorisé.";
--- mowadaba.ma/validation.js
+++ mowadaba.ma/validation.js
@@ -1,14 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Initialisation de Materialize
+  
     M.AutoInit();
 
-    // Validation du formulaire
     const form = document.getElementById('inscriptionForm');
 
     form.addEventListener('submit', function(event) {
-        // Valider les champs ici (par exemple, vérifier que l'e-mail est valide, etc.)
-
-        // Si la validation échoue, empêcher la soumission du formulaire
+        
         if (!validationReussie) {
             event.preventDefault();
         }
